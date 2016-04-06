a96decd620f98ba9b1fabe34ae30ec15f47c1717
ronan.mccabe <ronan.mccabe@27partners.com>
2016-04-06 01:13:47 +0100

    FEATURE: Big refactor and added docs.

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 # ooft
-front end image maker smaller
 
-outputFormat - 'jpeg' or 'png'  
+Takes an image file and shrinks it to the size of your liking, in a promise. Only usable on the front end at the moment due to it using several browser APIs, and will likely have trouble with any IE before 11. Also due to being 'required' it needs to be used alongside [`browserify`](https://www.npmjs.com/package/browserify) or [`webpack`](https://www.npmjs.com/package/webpack).
+
+### Example Usage:
+
+```
+var options = {
+
+  // Desired file size in MB.
+  // Required.
+  outputSize: 2,
+
+  // Percentage buffer acceptable for output size.
+  // Default is 10.
+  buffer: 10,
+
+  // Desired format, 'jpg' or 'png'.
+  // Default is 'jpg'.
+  format: 'jpg'
+
+};
+```
+```
+var ooft = require('ooft');
+
+ooft(file, options)
+.then(function(data) {
+  //http://stackoverflow.com/questions/21044798/how-to-use-formdata-for-ajax-file-upload
+  var formData = new FormData();
+  formData.append('image', data);
+
+  $.ajax({
+    method: 'POST',
+    url: '/upload',
+    data: formData,
+    // THIS MUST BE SET FOR FILE UPLOADING
+    contentType: false,
+    processData: false
+  });
+
+})
+.catch(function(err) {
+  console.log(err);
+});
+```
--- index.js
+++ index.js
@@ -1,51 +1,95 @@
-module.exports = function(source, outputFileSize, outputFormat) {
+'use strict';
+
+module.exports = function(source, options) {
 
   return new Promise(function(resolve, reject) {
 
-    var mimeType = setFileType(outputFormat);
+    var mimeType = setFileType(options.format);
     var quality = 95;
+
+    if (!options.outputSize) return resolve(source);
+
+    if (!source) return reject('ERROR: File must be provided.');
+
+    var targetSize = calculateDesiredFileSize(options.outputSize);
+
+    if (targetSize >= source.size) {
+      return resolve(source);
+    }
 
     convertImageToBase64(source).then(function(base64Image) {
 
-      if (outputFileSize === 'max') {
-        return resolve(base64Image);
-      }
-
-      outputFileSize *= 1000000; // Converting MB to B.
-      outputFileSize *= 1.1; // Add a ten percent buffer.
-
-      if (outputFileSize > source.size) {
-        return resolve(base64Image);
-      }
-
       getImageDimensions(base64Image).then(function(dimensions) {
-        var cvs = document.createElement('canvas');
+        var cvs      = document.createElement('canvas');
+        var context  = cvs.getContext('2d');
+        var imageObj = new Image();
 
         cvs.width = dimensions.width;
         cvs.height = dimensions.height;
 
-        var context = cvs.getContext('2d');
-        var imageObj = new Image();
-
         imageObj.onload = function() {
+
           context.drawImage(imageObj, 0, 0);
           var redrawnImage = cvs.toDataURL(mimeType, quality/100);
 
           // Base64 encoded images are 1.33 times larger than once they are saved to disk.
-          if (redrawnImage.length / 1.33 > outputFileSize) {
+          if (redrawnImage.length / 1.33 > targetSize) {
             quality -= 5;
             imageObj.src = base64Image;
           } else {
-            resolve(redrawnImage);
+            resolve(base64ToBlob(redrawnImage));
           }
+
         };
 
         imageObj.src = base64Image;
+
       });
 
     });
 
-  })
+  });
+
+  function calculateDesiredFileSize(size) {
+    size *= 1000000; // Converting to B to MB.
+
+    // 10% is default.
+    var buffer = options.buffer ? options.buffer / 100 : 0.1;
+
+    return Math.round(size *= 1 + buffer);
+  }
+
+  function base64ToBlob(b64Data) {
+    // http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+    function b64toBlob(b64Data, contentType, sliceSize) {
+      contentType = contentType || '';
+      sliceSize = sliceSize || 512;
+
+      var byteCharacters = atob(b64Data);
+      var byteArrays = [];
+
+      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+      }
+
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+    }
+
+    // remove leading data:image/jpeg;base64,
+    b64Data = b64Data.split(',')[1];
+
+    return b64toBlob(b64Data, 'image/jpg'); // or 'image/png'
+  }
 
   function getImageDimensions(image) {
     return new Promise(function(resolve, reject) {
@@ -53,13 +97,14 @@ module.exports = function(source, outputFileSize, outputFormat) {
 
       imageElement.onload = function() {
         resolve({height: imageElement.naturalHeight, width: imageElement.naturalWidth});
-      }
+      };
 
       imageElement.src = image;
     });
   }
 
   function convertImageToBase64(source) {
+    // if true return var base64Image = original_data.toString('base64');
     var fileReader = new FileReader();
 
     return new Promise(function(resolve, reject) {
@@ -68,7 +113,7 @@ module.exports = function(source, outputFileSize, outputFormat) {
 
         resolve(fileLoadedEvent.target.result);
 
-      }
+      };
 
       fileReader.readAsDataURL(source);
 
@@ -79,4 +124,4 @@ module.exports = function(source, outputFileSize, outputFormat) {
     return outputFormat === 'png' ? 'image/png' : 'image/jpeg';
   }
 
-}
+};
